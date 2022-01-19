3e3b68ab0b9572e9b6cc7f589ef66fce3b7b9659
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-01-18 23:23:47 -0600

    review routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+
 const userRoutes = require('./userRoutes');
 const strainRoutes = require('./strainRoutes');
+const reviewRoutes = require('./reviewRoutes');
 
 router.use('/users', userRoutes);
 router.use('/strains', strainRoutes);
+router.use('/reviews', reviewRoutes);
 
 module.exports = router;
