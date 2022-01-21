8a4e0e85bbc6cc5d6644e7d527dc7707515dd37b
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-01-20 20:05:20 -0600

    moved review form

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -21,7 +21,7 @@ router.get('/', withAuth, async (req, res) => {
         const userReviews = reviewData.filter(user => user.id === req.session.user_id)
 
         const reviews = userReviews[0].user_strains.map(data => {
-            return { strain_name: data.strain_name, comment: data.review.comment, rating: data.review.rating }
+            return { strain_name: data.strain_name, comment: data.review.comment, rating: data.review.rating, date_created: data.review.date_created }
         })
 
         if (!reviewData) {
@@ -75,28 +75,28 @@ router.get('/', withAuth, async (req, res) => {
 // });
 
 // myreviews create GET - http://localhost:3001/api/reviews/create
-router.get('/create', withAuth, async (req, res) => {
-    try {
-        const reviewData = await Review.findAll({
-            where: {
-                user_id: req.session.user_id
-            },
-        });
+// router.get('/create', withAuth, async (req, res) => {
+//     try {
+//         const reviewData = await Review.findAll({
+//             where: {
+//                 user_id: req.session.user_id
+//             },
+//         });
 
-        if (!reviewData) {
-            res.status(404).json({ message: 'No reviews found!' });
-            return;
-        }
-        const reviews = reviewData.map(review => review.get({ plain: true }));
-        res.render('create-review', {
-            reviews,
-            logged_in: req.session.logged_in
-        });
-        // res.status(200).json(reviewData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+//         if (!reviewData) {
+//             res.status(404).json({ message: 'No reviews found!' });
+//             return;
+//         }
+//         const reviews = reviewData.map(review => review.get({ plain: true }));
+//         res.render('create-review', {
+//             reviews,
+//             logged_in: req.session.logged_in
+//         });
+//         // res.status(200).json(reviewData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 // create a review in the db if logged in - http://localhost:3001/api/posts/
 router.post('/', withAuth, (req, res) => {
